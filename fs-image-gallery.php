de8b50b1dbae9499e2a4cf7219954a55fe9db084
Michael Wilson <michaelw@4sitestudios.com>
2026-01-04 18:03:27 -0500

    Bump plugin version to 1.0.1

--- fs-image-gallery.php
+++ fs-image-gallery.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       4Site Image Gallery
  * Description:       Provides an image gallery block for the Gutenberg editor.
- * Version:           1.0
+ * Version:           1.0.1
  * Requires at least: 6.7
  * Requires PHP:      7.4
  * Author:            MichaelW
